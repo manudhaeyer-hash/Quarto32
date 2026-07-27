1a1237348d226bfbc509c5cc8883bdce57515445
manudhaeyer-hash <manu.dhaeyer@gmail.com>
2026-07-28 00:45:01 +0200

    Remove private config and test files from repo tracking

--- run.ps1
+++
@@ -1,5 +0,0 @@
-$env:JAVA_HOME="C:\Program Files\Eclipse Adoptium\jdk-25.0.3.9-hotspot"
-# Boss = solver C++ (config/Boss.cpp). Necessite g++ (MinGW-w64) dans le PATH.
-g++ -O2 -o config/boss.exe config/Boss.cpp
-mvn clean install
-mvn exec:exec -D"exec.executable"="java" -D"exec.classpathScope"="test" -D"exec.args"="--add-opens java.base/java.lang=ALL-UNNAMED -cp %classpath Main"
